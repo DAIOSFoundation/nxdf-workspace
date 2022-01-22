0c69c3300663e6033841cc979208d4a66f85ca6c
spock-mark1 <spockwoz@gmail.com>
2022-01-23 02:32:06 +0900

    added mongo connection(temp)

--- apps/nxdf-api/src/app/app.module.ts
+++ apps/nxdf-api/src/app/app.module.ts
@@ -10,15 +10,18 @@ import { SolanaModule } from './modules/solana/solana.module';
 
 @Module({
   imports: [
-    // MongooseModule.forRootAsync({
-    //   useFactory: () => (
-    //     {
-    //       uri: '',  // environment.database.uri
-    //       useNewUrlParser: true,
-    //     }
-    //   ),
-    //   connectionName: 'default'
-    // }),
+    MongooseModule.forRootAsync({
+      useFactory: () => (
+        {
+          // uri: 'mongodb://root:20ahdrh49db5qlalf6qjsghgg@34.64.183.133:30001',  // environment.database.uri
+          uri: 'mongodb+srv://llap-dev-1:XbS5geycSszfrPd@cluster0.mhq0y.mongodb.net/nxdf?retryWrites=true&w=majority',
+          // environment.database.uri
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+        }
+      ),
+      connectionName: 'default'
+    }),
 
     SolanaModule
   ],
