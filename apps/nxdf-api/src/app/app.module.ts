8afedad2461e94ba60fa196b1877fcab4513766d
spock-mark1 <spockwoz@gmail.com>
2022-01-22 22:04:53 +0900

    init solana nft api

--- apps/nxdf-api/src/app/app.module.ts
+++ apps/nxdf-api/src/app/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { SolanaModule } from './modules/solana/solana.module';
+
+
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRootAsync({
+    //   useFactory: () => (
+    //     {
+    //       uri: '',  // environment.database.uri
+    //       useNewUrlParser: true,
+    //     }
+    //   ),
+    //   connectionName: 'default'
+    // }),
+
+    SolanaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
